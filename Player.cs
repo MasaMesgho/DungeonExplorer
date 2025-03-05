7309d51ac8e6ae758bb582e18eb5737064aa0f34
MasaMesgho <james_sam_whittingham@outlook.com>
2025-03-05 20:39:58 +0000

    added comments to the player class

--- Player.cs
+++ Player.cs
@@ -5,6 +5,8 @@ namespace DungeonExplorer
 {
     public class Player
     {
+        // this class contains all the player information and actions.
+        // below are the needed variables
         public string Name { get; private set; }
         public int Health { get; private set; }
         private Dictionary <string,int> inventory = new Dictionary<string,int>();
@@ -13,9 +15,13 @@ namespace DungeonExplorer
         {
             Name = name;
             Health = 100;
+            // assigns the name the user enters from the game loop, then assigns the starting health (100)
         }
         public void PickUpItem(string item)
         {
+            // adds an item to the players inventory
+            // if the item is already in the inventory adds 1 to the dictionary int value to show the ammount
+            // if the item is not in the inventory creates a new dictionary item for it.
             if (this.inventory.ContainsKey(item))
             {
                 this.inventory[item]++;
@@ -27,9 +33,12 @@ namespace DungeonExplorer
         }
         public string InventoryContents()
         {
+            // returns the inventory contents.
+            // first checks if the inventory is empty
             string outputString;
             if (this.inventory.Count != 0)
             {
+                // if the inventory is not empty, loops through all items and their ammunts and passes it back to the game loop
                 outputString = "";
                 foreach (var i in this.inventory)
                 {
@@ -39,6 +48,7 @@ namespace DungeonExplorer
             }
             else
             {
+                // if the inventory is empty, returns that information.
                 outputString = "Empty";
             }
             return outputString;
