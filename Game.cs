a234467a93030a6a55e76a30b2b0b72751ed9182
MasaMesgho <james_sam_whittingham@outlook.com>
2025-03-05 18:37:35 +0000

    moved player actions to new method for ease of reading.

--- Game.cs
+++ Game.cs
@@ -11,18 +11,18 @@ namespace DungeonExplorer
     {
         public Random rnd = new Random();
         public Player player;
-        private Room currentRoom = new Room(0);
+        private Room currentRoom;
 
         public Game()
         {
             // Initialize the game with one room and one player
+            currentRoom = new Room(0);
+            Console.Write("Please Enter your name: ");
+            string name = Console.ReadLine();
+            player = new Player(name);
         }
         public void Start()
         {
-            string name;
-            Console.WriteLine("Please Enter your name");
-            name = Console.ReadLine();
-            player = new Player(name);
             // Change the playing logic into true and populate the while loop
             bool playing = true;
             while (playing)
@@ -30,64 +30,82 @@ namespace DungeonExplorer
                 // Code your playing logic here
                 Console.WriteLine(currentRoom.getDescription());
                 Console.WriteLine("The commands are: left, forward, right, inventory, health, search, exit");
-                bool waitingInput = true;
-                while (waitingInput)
+                bool moving = false;
+                while (!moving)
                 {
                     string userInput = Console.ReadLine();
-                    userInput = userInput.ToLower();
-                    switch (userInput)
+                    string result = action(userInput.ToLower());
+                    switch (result)
                     {
-                        case "left":
-                            currentRoom = new Room();
-                            waitingInput = false;
+                        case "false":
                             break;
-
-                        case "forward":
-                            currentRoom = new Room();
-                            waitingInput = false;
+                        case "true":
+                            moving = true;
                             break;
-
-                        case "right":
-                            currentRoom = new Room();
-                            waitingInput = false;
-                            break;
-
-                        case "inventory":
-                            Console.WriteLine("{0}", player.InventoryContents());
-                            break;
-
-                        case "health":
-                            Console.WriteLine("Current Health: {0}", player.Health);
-                            break;
-
                         case "exit":
+                            moving = true;
                             playing = false;
-                            waitingInput = false;
-                            break;
-
-                        case "search":
-                            string contents = currentRoom.getItem();
-                            if (contents=="None")
-                            {
-                                Console.WriteLine("The room is Empty.");
-                            }
-                            else if (contents=="checked")
-                            {
-                                Console.WriteLine("You have already searched this room.");
-                            }
-                            else
-                            {
-                                Console.WriteLine("You have found 1 x {0}!", contents);
-                                player.PickUpItem(contents);
-                            }
-                            break;
-
-                        default:
-                            Console.WriteLine("Command not recognised");
                             break;
                     }
+                    
                 }
             }
+        }
+
+        private string action(string userInput)
+        {
+            string output = "false";
+            switch (userInput)
+            {
+                case "left":
+                    currentRoom = new Room();
+                    output = "true";
+                    break;
+
+                case "forward":
+                    currentRoom = new Room();
+                    output = "true";
+                    break;
+
+                case "right":
+                    currentRoom = new Room();
+                    output = "true";
+                    break;
+
+                case "inventory":
+                    Console.WriteLine("{0}", player.InventoryContents());
+                    break;
+
+                case "health":
+                    Console.WriteLine("Current Health: {0}", player.Health);
+                    break;
+
+                case "exit":
+                    output = "exit";
+                    break;
+
+                case "search":
+                    string contents = currentRoom.getItem();
+                    if (contents == "None")
+                    {
+                        Console.WriteLine("The room is Empty.");
+                    }
+                    else if (contents == "checked")
+                    {
+                        Console.WriteLine("You have already searched this room.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You have found 1 x {0}!", contents);
+                        player.PickUpItem(contents);
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Command not recognised");
+                    break;
+            }
+            return output;
         }
     }
 }
