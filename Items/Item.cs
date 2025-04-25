522005a9da13aa7cd9a266818823fc4615b2c061
MasaMesgho <james_sam_whittingham@outlook.com>
2025-04-25 14:24:11 +0100

    Changed item to be a abstract class added IUsable interface for using items added potion class

---
+++ Items/Item.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DungeonExplorer
+{
+
+    public abstract class Item : IUsable
+    {
+        // all items will need an ID, Name and Description
+        // these are all protected values as I do not want them changed outside of the class
+        // they have a public version so that I can retrieve the values outside of the class
+        protected int ID;
+        public int iD
+        {
+            get { return ID; }
+            protected set { ID = value; }
+        }
+
+        protected string Name;
+        public string name
+        {
+            get { return Name; }
+            protected set { Name = value; }
+        }
+        protected string Description;
+        public string description
+        {
+            get { return Description; }
+            protected set { Description = value; }
+        }
+        // constuctors will be unique to the type of item so an empty constructor is shown here
+        public Item() { }
+        // use method to be compliant with interface.
+        public abstract void Use(Player player);
+
+
+    }
+}
---
+++ Items/Potion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer
+{
+    class Potion : Item
+    {
+        // adds in a restores value to the potion
+        protected int Restores;
+
+        /// <summary>
+        /// creates an instance of the potion.
+        /// </summary>
+        public Potion(int level)
+        {
+            this.name = "Health Potion";
+            // if the health potion is better than the basic (level 1) shows how advanced it is.
+            if (level > 1) this.name += "+ " + (level - 1);
+            this.iD = 1;
+            this.Restores = 10 + level*5;
+            this.description = $"A Level {level} Health Potion, it makes you feel better (restores {Restores} health)";
+        }
+        /// <summary>
+        /// uses the potion, restores the stored amount of health to the player
+        /// </summary>
+        /// <param name="player"> the current player </param>
+        public override void Use(Player player)
+        {
+            player.changeHealth(Restores);
+        }
+
+    }
+}
