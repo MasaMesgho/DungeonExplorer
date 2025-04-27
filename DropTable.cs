60e59d07ae67c833c21fe5cb30ee5714d71d3b7a
MasaMesgho <james_sam_whittingham@outlook.com>
2025-04-27 18:35:37 +0100

    Drop Table commented

--- DropTable.cs
+++ DropTable.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.CodeDom;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,7 @@ namespace DungeonExplorer
     public class DropTable
     {
         // public so that it can be checked for testing purposes
+        // private set as i only want it changed within the class
         public List<int> Table
         {
             get;
@@ -25,9 +27,15 @@ namespace DungeonExplorer
 
         public DropTable (TableType type, int level)
         {
-
+            Level = level;
+            Table = GenerateTable(type);
         }
 
+        /// <summary>
+        /// Generates the Drop Table depending on they type of drop table wanted
+        /// </summary>
+        /// <param name="type">Enemy or room</param>
+        /// <returns>returns the generated table</returns>
         private List<int> GenerateTable(TableType type)
         {
             List<int> table = new List<int>();
@@ -52,18 +60,27 @@ namespace DungeonExplorer
             }
             return table;
         }
-
+        /// <summary>
+        /// gets an item from the drop table and returns it, or null if no item.
+        /// </summary>
+        /// <returns>the Item or null</returns>
         public Item GetDrop()
         {
+            // Uses rnd class to generate a random number between 0 and 9
+            // uses the result to generate a random item and returns it.
             int result = Program.rnd.Next(0, 9);
+            // returns an item or null if no item is generated
             switch (result)
             {
                 case 0:
-                    return default;
+                    return null;
                 case 1:
                     return new Potion(Level);
                 case 2:
                     return new Sword(Level);
+                default:
+                    Console.WriteLine("Something Went Wrong with generating an item.");
+                    return null;
             }
 
         }
