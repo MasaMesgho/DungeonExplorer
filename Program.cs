1e39dcb5fad1666c333fd0f38e67e19486992597
MasaMesgho <james_sam_whittingham@outlook.com>
2025-05-01 14:42:34 +0100

    fixed issue

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace DungeonExplorer
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            // see if the player wants to test before running.
             Console.WriteLine("Welcome To Dungeon Explorer \nPress 'y' to run tests or any other key to start playing.");
             char input = Console.ReadKey().KeyChar;
             if (input == 'y' || input == 'Y')
@@ -24,6 +25,7 @@ namespace DungeonExplorer
                 // call the test and then remove the object after testing is complete.
                 Testing test = new Testing();
                 test = null;
+                Console.Clear();
             }
             // starts the game loop
             Game game = new Game();
